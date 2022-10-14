3c40bf1ef0500062fd9aec41032bb80199737a76
SRGO88 <105660519+SRGO88@users.noreply.github.com>
2022-10-14 13:11:24 +0530

    Update drop.js

--- DragDropRichText/drop.js
+++ DragDropRichText/drop.js
@@ -1,9 +1,69 @@
-function allowDrop(ev) {
-    //disable browser default behaviour
-    ev.preventDefault();
- }
+const boxes = document.querySelectorAll('[data-dropzone]');
+
+boxes.forEach(box => {
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+});
+
+function dragEnter(ev) {
+  //disable browser default behaviour
+  ev.preventDefault();
+}
+
+function dragOver(ev) {
+  //disable browser default behaviour
+  ev.preventDefault();
+}
+
+function dragLeave(ev) {
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  console.log(data);
+  console.log(ev.target.nodeName);
+  var isLeft = 'text' == data || "image" == data;
  
- function drag(ev) {
+  switch(data)
+        {
+          case "text": 
+          var textEle = document.createElement('p');
+          textEle.contentEditable = true;
+          textEle.innerHTML = 'Add Text here';
+          ev.target.appendChild(textEle);
+          textEle.id = "";
+          break;
+
+          case "image":
+            var imgEle = document.createElement('img');
+                         imgEle.width = 400;
+                         imgEle.height = 150;
+                         imgEle.id = "img1";
+                         imgEle.src = "image_icon.jpg";
+                         imgEle.addEventListener("click", function(e) {
+                           hideOrShow();
+                        })
+                         ev.target.appendChild(imgEle);
+          break;
+
+          default: break;
+        }
+  
+  ev.stopPropagation();
+  return false;
+}
+ 
+
+const draggableItems = document.querySelectorAll('[data-draggable]');
+
+draggableItems.forEach(draggableItem => {
+  draggableItem.addEventListener('dragstart', dragStart);
+});
+
+ function dragStart(ev) {
    console.log("drag start.");
     ev.dataTransfer.setData("text",ev.target.classList[0]);
  }
@@ -13,44 +73,6 @@ function allowDrop(ev) {
     node.parentNode.removeChild(node);
   }
  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    console.log(data);
-    console.log(ev.target.nodeName);
-    var isLeft = 'text' == data || "image" == data;
-   
-
-
-    switch(data)
-          {
-            case "text": 
-            var textEle = document.createElement('p');
-            textEle.contentEditable = true;
-            textEle.innerHTML = 'Add Text here';
-            ev.target.appendChild(textEle);
-            textEle.id = "";
-            break;
-
-            case "image":
-              var imgEle = document.createElement('img');
-                           imgEle.width = 400;
-                           imgEle.height = 150;
-                           imgEle.id = "img1";
-                           imgEle.src = "image_icon.jpg";
-                           imgEle.addEventListener("click", function(e) {
-                             hideOrShow();
-                          })
-                           ev.target.appendChild(imgEle);
-            break;
-
-            default: break;
-          }
-    
-    ev.stopPropagation();
-    return false;
-  }
-
   function getTemplateCode(){
 
   }
