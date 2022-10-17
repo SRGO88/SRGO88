5b4291ba8d289c371cdb968d0cf5d1e698f16d8e
SRGO88 <105660519+SRGO88@users.noreply.github.com>
2022-10-17 15:08:17 +0100

    Update style.js

--- DragDropRichText/style.js
+++ DragDropRichText/style.js
@@ -1,25 +1,64 @@
+//Variables
+
 var Content = "Content";
 var Layouts = "Layouts";
-var btns = document.querySelectorAll("[data-selectbutton]");
+var tabElements = document.querySelectorAll("[data-selectbutton]");
+var imgElements = document
+    .querySelector("section.blockTabTemplate > table")
+    .querySelectorAll("img");
 
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(e) {
-        var currentButton = document.querySelectorAll("button.active");
-        currentButton[0].className = currentButton[0].className.replace("active", "");
-        this.className += "active";
-        let data = this.innerText;
-        switch (data) {
-            case Layouts:
-                document.querySelectorAll("[data-Layouts]")[0].classList.remove("hidden");
-                document.querySelectorAll("[data-Content]")[0].classList.add("hidden");
-                break;
-            case Content:
-                document.querySelectorAll("[data-Layouts]")[0].classList.add("hidden");
-                document.querySelectorAll("[data-Content]")[0].classList.remove("hidden");
-                break;
-            default:
-                break;
-        }
+//Common functions
 
-    });
+function addClass(querySelector, className) {
+    document.querySelectorAll(querySelector)[0].classList.add(className);
 }
+
+function removeClass(querySelector, className) {
+    document.querySelectorAll(querySelector)[0].classList.remove(className);
+}
+
+function replaceClass(querySelector, before, after) {
+    let elementClass = document.querySelectorAll(querySelector)[0];
+    elementClass.className = elementClass.className.replace(before, after);
+}
+
+function addClickEventToElements(element, eventName, functionName) {
+    element.addEventListener(eventName, functionName);
+}
+
+//Event Function declaration
+
+let buttonClick = function(e) {
+    replaceClass("button.active", "active", "");
+    this.classList.add("active");
+    let data = this.innerText;
+    switch (data) {
+        case Layouts:
+            removeClass("[data-section_Layouts]", "hidden");
+            addClass("[data-section_Content]", "hidden");
+            addClass("[data-section_editImage]", "hidden");
+            break;
+        case Content:
+            addClass("[data-section_Layouts]", "hidden");
+            removeClass("[data-section_Content]", "hidden");
+            addClass("[data-section_editImage]", "hidden");
+            break;
+        default:
+            break;
+    }
+};
+
+let selectImage = function(e) {
+    addClass("[data-section_Layouts]", "hidden");
+    addClass("[data-section_Content]", "hidden");
+    removeClass("[data-section_editImage]", "hidden");
+};
+
+//Event initialized functions
+tabElements.forEach((btns) => {
+    addClickEventToElements(btns, "click", buttonClick);
+});
+
+imgElements.forEach((img) => {
+    img.addEventListener(img, "click", selectImage);
+});
